777facce09e2a2bcc1032b92360fbc56b507eb3a
Arjun S <arjun8107@gmail.com>
2023-10-06 22:30:41 +0530

    fix(ToastUI): Fixed toast transparency

--- apps/webapp/app/components/primitives/Toast.tsx
+++ apps/webapp/app/components/primitives/Toast.tsx
@@ -17,20 +17,9 @@ export function Toast() {
     }
     const { message, type, options } = toastMessage;
 
-    switch (type) {
-      case "success":
-        toast.custom((t) => <ToastUI variant="success" message={message} t={t as string} />, {
-          duration: options.ephemeral ? defaultToastDuration : permanentToastDuration,
-        });
-        break;
-      case "error":
-        toast.custom((t) => <ToastUI variant="error" message={message} t={t as string} />, {
-          duration: options.ephemeral ? defaultToastDuration : permanentToastDuration,
-        });
-        break;
-      default:
-        throw new Error(`${type} is not handled`);
-    }
+    toast.custom((t) => <ToastUI variant={type} message={message} t={t as string} />, {
+      duration: options.ephemeral ? defaultToastDuration : permanentToastDuration,
+    });
   }, [toastMessage]);
 
   return <Toaster />;
@@ -46,22 +35,24 @@ export function ToastUI({
   t: string;
 }) {
   return (
-    <div
-      className="flex gap-2 rounded-lg border border-slate-750 bg-no-repeat p-4 text-bright shadow-md"
-      style={{
-        background:
-          "radial-gradient(at top, hsla(271, 91%, 65%, 0.18), hsla(221, 83%, 53%, 0.18)) hsla(221, 83%, 53%, 0.18)",
-      }}
-    >
-      {variant === "success" ? (
-        <CheckCircleIcon className="h-6 w-6 text-green-600" />
-      ) : (
-        <ExclamationCircleIcon className="h-6 w-6 text-rose-600" />
-      )}
-      {message}
-      <button className="p-1" onClick={() => toast.dismiss(t)}>
-        <XMarkIcon className="h-4 w-4 text-bright" />
-      </button>
+    <div className="rounded-lg border border-slate-750 bg-midnight-900 shadow-md">
+      <div
+        className="flex gap-2 rounded-lg bg-no-repeat p-4 text-bright"
+        style={{
+          background:
+            "radial-gradient(at top, hsla(271, 91%, 65%, 0.18), hsla(221, 83%, 53%, 0.18)) hsla(221, 83%, 53%, 0.18)",
+        }}
+      >
+        {variant === "success" ? (
+          <CheckCircleIcon className="h-6 w-6 text-green-600" />
+        ) : (
+          <ExclamationCircleIcon className="h-6 w-6 text-rose-600" />
+        )}
+        {message}
+        <button className="p-1" onClick={() => toast.dismiss(t)}>
+          <XMarkIcon className="h-4 w-4 text-bright" />
+        </button>
+      </div>
     </div>
   );
 }
